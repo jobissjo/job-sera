96756669c1ecb48677cdce2bac337539733138b8
Jobi S S <jobisjobi1234@gmail.com>
2024-01-09 22:12:35 +0530

    2nd commit

--- job-sera/src/app/app-routing.module.ts
+++ job-sera/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-  const routes: Routes = [
-    {
-      path: '',
-      loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-    }
-  ];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'jobs',
+    loadChildren: () => import('./modules/jobs/jobs.module').then(m => m.JobsModule)
+  }
+];
 
 
 @NgModule({
