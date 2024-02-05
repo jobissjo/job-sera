b35946125332876ce3da3cb52dead3e8da5c9091
Jobi S S <jobisjobi1234@gmail.com>
2024-02-05 21:59:04 +0530

    created 'create-job-html page' and changed theme color

--- job-sera/src/app/modules/employer/components/create-job/create-job.component.ts
+++ job-sera/src/app/modules/employer/components/create-job/create-job.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-job',
@@ -7,29 +7,69 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-job.component.scss']
 })
 export class CreateJobComponent {
-
-  
+ 
 
   createJobForm:FormGroup;
+  qualificationArray!:FormArray;
+  skillsArray!:FormArray;
+  responsibilityArray!:FormArray;
+
   constructor(private _formBuilder: FormBuilder) {
     this.createJobForm = this._formBuilder.group({
       jobTitle: ['', [Validators.required]],
       company: ['', [Validators.required]],
       location: ['', [Validators.required]],
       description: ['', [Validators.required]],
-      qualification: this._formBuilder.array([]),
-      skills: this._formBuilder.array([]),
-      salary: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
       shift:[''],
       jobType:[''],
+      salary: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+      experience:['', Validators.required],
+      qualification: this._formBuilder.array([]),
+      skills: this._formBuilder.array([]),
       responsibilities:this._formBuilder.array([]),
-      experience:['', Validators.required]
+      
       // Add more form controls as needed
     });
   }
 
-  onCreateJobSubmit(){
+  ngOnInit(){
+    this.qualificationArray = <FormArray> this.createJobForm.get('qualification');
+    this.skillsArray = <FormArray> this.createJobForm.get('skills')
+    this.responsibilityArray = <FormArray> this.createJobForm.get('responsibilities')
+  }
 
+  onCreateJobSubmit(){
+    console.log(this.createJobForm.value);
+  }
+
+  addQualification(){
+    this.qualificationArray.push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  deleteQualification(index:number){
+    this.qualificationArray.removeAt(index);
+  }
+
+  addSkill(){
+    this.skillsArray.push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  deleteSkill(index:number){
+    this.skillsArray.removeAt(index);
+  }
+
+  addResponsibility(){
+    this.responsibilityArray.push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  deleteResponsibility(index:number){
+    this.responsibilityArray.removeAt(index)
   }
 
 
