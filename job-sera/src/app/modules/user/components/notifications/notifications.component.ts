a7504ec0157ed527589140d728fa7dac6a17ef4a
Jobi S S <jobisjobi1234@gmail.com>
2024-02-09 22:36:44 +0530

    just showing up 09-02-2024

--- job-sera/src/app/modules/user/components/notifications/notifications.component.ts
+++ job-sera/src/app/modules/user/components/notifications/notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotificationType } from 'src/app/shared/Models/notification.type';
 
 @Component({
   selector: 'app-notifications',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent {
+
+  notifications: NotificationType[] = [
+    {
+      title: "Notification 1",
+      message: "This is notification 1.",
+      from: "User 1",
+      date: "2024-02-02"
+    },
+    {
+      title: "Notification 2",
+      message: "This is notification 2.",
+      from: "User 2",
+      date: "2024-02-03"
+    },
+    // Add more objects as needed
+  ];
 
 }
---
+++ job-sera/src/app/shared/Models/notification.type.ts
@@ -0,0 +1,6 @@
+export interface NotificationType{
+    title:string,
+    message:string,
+    from:string,
+    date:string
+}
