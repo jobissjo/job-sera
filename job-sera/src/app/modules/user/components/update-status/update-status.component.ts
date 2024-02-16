cfb754e3df1b74b89b386739536b350ab056d9ef
Jobi S S <jobisjobi1234@gmail.com>
2024-02-16 23:11:58 +0530

    update-status completed

--- job-sera/src/app/modules/user/components/update-status/update-status.component.ts
+++ job-sera/src/app/modules/user/components/update-status/update-status.component.ts
@@ -8,15 +8,23 @@ import { MyJobs } from '../../models/my-jobs';
 })
 export class UpdateStatusComponent {
 
-  @Output() closeUpdateStatus:EventEmitter<null> = new EventEmitter<null>();
- @Input() selectedUpdateJobStatus!:MyJobs;
- ngOnInit(){
-  console.log(this.selectedUpdateJobStatus);
-  
- }
+  @Output() closeUpdateStatus: EventEmitter<null> = new EventEmitter<null>();
+  @Input() selectedUpdateJobStatus!: MyJobs;
+  ngOnInit() {
+    console.log(this.selectedUpdateJobStatus);
 
- onCloseDetail(){
-  this.closeUpdateStatus.emit()
- }
+  }
+
+  onCloseDetail() {
+    this.closeUpdateStatus.emit()
+  }
+
+  applicationStatus: { title: string, icon: string, color:string, status:string }[] = [
+    { title:"Interviewing", icon:'receipt_list',color:"accent", status:"interviewing"},
+  { title:"Offer Received", icon:'thumb_up',color:"primary", status:"offer received"},
+    { title:"Hired", icon:'person',color:"primary", status:"hired"},
+    { title:"Not Selected by Employer", icon:'close',color:"warn", status:"not selected"},
+    { title:"No Longer Interested", icon:'thumb_down',color:"warn", status:"rejected"}
+  ]
 
 }
